735e3789fded718a317d619958c859ee808a2233
luopengift <870148195@qq.com>
2024-04-24 02:25:56 +0800

    update

--- server.go
+++ server.go
@@ -4,12 +4,9 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/http/pprof"
-	"os"
-	"path"
 	"strings"
 )
 
@@ -18,51 +15,6 @@ var ErrHandler = func(err string, code int) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err, code)
 	})
-}
-
-// ServeUpload serve upload handler.
-// curl http://127.0.0.1:8080/_upload H 'Content-Type: multipart/form-data' -F '/abc/123=@xxx.txt' -F '456=@abc/xyz.txt'
-// upload files is $perfix/abc/123/xxx.txt and $perfix/456/xyz.txt
-func ServeUpload(prefix string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		reader, err := r.MultipartReader()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		save := func(prefix, dir, file string, r io.Reader) error {
-			if file == "" { // this is FormData
-				data, err := io.ReadAll(r)
-				fmt.Printf("FormData=[%s]\n", string(data))
-				return err
-			}
-			paths := path.Join(prefix, dir)
-			if err := os.MkdirAll(paths, 0755); err != nil {
-				return err
-			}
-			dst, err := os.Create(path.Join(paths, file))
-			if err != nil {
-				return err
-			}
-			defer dst.Close()
-			_, err = io.Copy(dst, r)
-			return err
-		}
-
-		for {
-			part, err := reader.NextPart()
-			if err == io.EOF {
-				break
-			}
-			//fmt.Printf("FileName=[%s], FormName=[%s]\n", part.FileName(), part.FormName())
-			if err := save(prefix, part.FormName(), part.FileName(), part); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-		_, _ = fmt.Fprintf(w, "Successfully Uploaded File\n")
-	}
 }
 
 // WarpHttpHandler warp `http.Handler`.
